7a71463fb7dddb70c3a8a5d1cde69608f68127a3
Liraz Shay <liraz@om2.com>
2019-01-23 16:25:06 +0200

    fix EditTask because html was changed

--- TodoMVC.PageObjects/TaskRow.cs
+++ TodoMVC.PageObjects/TaskRow.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using System.Runtime.Serialization.Formatters;
+using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
 
 namespace TodoMVC.PageObjects
 {
+  
+
     public class TaskRow
     {
         private readonly IWebDriver driver;
         private readonly IWebElement rowElement;
         private IWebElement CompletedCheckbox => rowElement.FindElement(By.CssSelector("input[ng-model='todo.completed']"));
+
 
         public TaskRow(IWebElement rowElement,IWebDriver driver)
         {
@@ -18,23 +23,21 @@ namespace TodoMVC.PageObjects
 
         public string TaskText => rowElement.Text;
         public bool IsCompleted => rowElement.GetAttribute("class").Contains("completed");
-        
+
         public void EditTask(string newName)
         {
+            //double click by Actions library
             var editable = rowElement.FindElement(By.CssSelector("[ng-dblclick='editTodo(todo)']"));
             Actions actions = new Actions(driver);
-            actions.MoveToElement(editable)
-                .DoubleClick()
-                .Build()
-                .Perform();
+            actions.MoveToElement(editable).DoubleClick().Build().Perform();
+            //select all text, delete it and type new text and press enter
             var input = rowElement.FindElement(By.CssSelector("form input.edit"));
-            actions
-                .MoveToElement(input)
-                .SendKeys(Keys.Control + "a")
-                .SendKeys(Keys.Delete)
-                .SendKeys(newName + Keys.Enter)
-                .Build().Perform();
+            actions.MoveToElement(input).Build().Perform();
+            actions.SendKeys(Keys.Control + "a").SendKeys(Keys.Delete).Build().Perform();
+            input.SendKeys("");
+            input.SendKeys(newName+ Keys.Enter);
         }
+
 
         public void DeleteTask()
         {
